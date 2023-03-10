2366a6173f2a8415eb3ba77b0f87c063ce936f82
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-11 01:49:40 +0700

    belajar select channel dengan perulangan

--- channel_test.go
+++ channel_test.go
@@ -92,3 +92,28 @@ func TestRangeChannel(t *testing.T) {
 		fmt.Println("Menerima data", data)
 	}
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break // harus buat break agar tidak deadlock
+		}
+	}
+}
