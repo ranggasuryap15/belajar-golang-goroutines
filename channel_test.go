2528142c38b0cca75a7d29fc6439becaf75f70ba
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-10 14:43:09 +0700

    belajar channel in dan out

--- channel_test.go
+++ channel_test.go
@@ -19,3 +19,39 @@ func TestCreateChannel(t *testing.T) {
 	data := <-channel
 	fmt.Println(data)
 }
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	close(channel)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Rangga Surya Prayoga"
+	fmt.Println("Selesai mengirim data ke channel:", channel)
+}
+
+func OnlyIn(channel chan<- string) { // untuk mengirim data
+	time.Sleep(2 * time.Second)
+	channel <- "Rangga Surya Prayoga"
+}
+
+func OnlyOut(channel <-chan string) { // untuk menerima data
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOut(t *testing.T) {
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(3 * time.Second)
+	close(channel)
+}
