54ceb9721d35a315ed6b170cc4e59938cb22d1c2
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-11 01:22:09 +0700

    belajar range channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package belajar_golang_gouroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -75,4 +76,19 @@ func TestBufferedChannel(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel) // wajib close channel agar tidak kena deadlock
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
 }
