1c186673676e19f29aea1c86356df96ecb9af741
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-11 20:11:15 +0700

    sync.Map yang aman dari race condition

---
+++ map_test.go
@@ -0,0 +1,31 @@
+package belajar_golang_gouroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+// semua package yang ada dalam sync itu aman dari race condition
+func AddToMap(data *sync.Map, value int, group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+	data.Store(value, value)
+}
+
+func TestMap(t *testing.T) {
+	data := &sync.Map{}
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go AddToMap(data, i, group)
+	}
+
+	group.Wait()
+
+	data.Range(func(key, value interface{}) bool {
+		fmt.Println(key, ":", value)
+		return true
+	})
+}
